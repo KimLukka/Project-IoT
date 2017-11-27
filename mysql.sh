dd7be46e62c55b6e7c7c6a844b4776a7e9b589c6
kaunderi <a.makelaa@gmail.com>
2017-11-27 10:01:00 +0200

    Security update

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-mosquitto_sub -h m23.cloudmqtt.com -u xxx -P xxx -p xxx -t 'esp/test' | while read RAW_DATA
+mosquitto_sub -h m14.cloudmqtt.com -u fbaeyadu -P 8IEoPV8J5yDI -p 17353 -t 'esp/test' | while re$
 do
 NIMI=$(echo $RAW_DATA | cut -f1 -d" ")
 VIESTI=$(echo $RAW_DATA | cut -f 2 -d" ")
 
+filename=/home/projekti/bin/user_password.txt
+user=$(cat $filename | cut -f1 -d" ")
+password=$(cat $filename | cut -f 2 -d" ")
 
-mysql -u esp8266 -pxxxxxxx <<TESTI
+mysql -u $user -p$password <<TESTI
 USE esp8266
-INSERT INTO esps VALUES ("$NIMI", "$VIESTI")
+INSERT INTO esps (mode, action) VALUES ("$NIMI", "$VIESTI")
+
 TESTI
 
 done
